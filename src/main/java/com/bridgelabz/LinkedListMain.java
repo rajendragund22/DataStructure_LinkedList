63ed1af1c14a11ab20903ddd8551d52ae098a494
RajendraGund <rajendragund2015@gmail.com>
2021-09-11 00:23:28 +0530

    Added : Program for the Adding the data at the Ending 56>30>70

--- src/main/java/com/bridgelabz/LinkedListMain.java
+++ src/main/java/com/bridgelabz/LinkedListMain.java
@@ -9,11 +9,16 @@ public class LinkedListMain {
         System.out.println("Welcome to the LinkedList Code !!");
         Scanner scanner = new Scanner(System.in);
         System.out.println("Press '1' to add the data at the start");
+        System.out.println("Press '2' to add the data at the end");
         switch (scanner.nextInt()) {
-            case 1: {
+            case 1:
                 Operations.addDataAtStart();
-            }
+                break;
+            case 2:
+                Operations.addDataAtEnd();
+                break;
         }
 
     }
+
 }
--- src/main/java/com/bridgelabz/Operations.java
+++ src/main/java/com/bridgelabz/Operations.java
@@ -7,8 +7,14 @@ public class Operations {
         linkedList.push(30);
         linkedList.push(56);
         linkedList.print();
+    }
 
-
+    public static void addDataAtEnd() {
+        LinkedList linkedList = new LinkedList();
+        linkedList.append(56);
+        linkedList.append(30);
+        linkedList.append(70);
+        linkedList.print();
     }
 }
 
