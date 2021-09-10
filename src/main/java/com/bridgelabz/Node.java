0b62a9d99a0077c639aa7c4585a06fa18cd1207e
RajendraGund <rajendragund2015@gmail.com>
2021-09-11 00:14:54 +0530

    Added : Program for the adding data at the starting 56>30>70

--- src/main/java/com/bridgelabz/Node.java
+++ src/main/java/com/bridgelabz/Node.java
@@ -3,8 +3,9 @@ package com.bridgelabz;
 public class Node {
     public int data;
     public Node next;
-    public Node(int data){
-        this.data=data;
-        this.next=null;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
     }
 }
