936c6e6b7475b9e8196872fad19d40a09a642e4d
RajendraGund <rajendragund2015@gmail.com>
2021-09-10 23:48:33 +0530

    Added : Program for the Create a LinkedList and add 56, 30 and 70

--- src/main/java/com/bridgelabz/LinkedListMain.java
+++ src/main/java/com/bridgelabz/LinkedListMain.java
@@ -1,7 +1,23 @@
 package com.bridgelabz;
 
 public class LinkedListMain {
+
+
     public static void main(String[] args) {
         System.out.println("Welcome to the LinkedList Code !!");
+        Node firstNode = new Node(56);
+        Node secondNode = new Node(30);
+        Node thirdNode = new Node(70);
+
+        Node head = firstNode;
+        firstNode.next = secondNode;
+        secondNode.next = thirdNode;
+        Node tail = thirdNode;
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.data );
+            temp = temp.next;
+        }
     }
+
 }
